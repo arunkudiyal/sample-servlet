b34302be4018dc3fc65067165c33a5e07d13d7fc
arunkudiyal <arunkudiyal@icloud.com>
2026-02-02 20:06:55 +0530

    Upadtes

--- src/main/java/com/company/myservlet/RegistrationServlet.java
+++ src/main/java/com/company/myservlet/RegistrationServlet.java
@@ -14,18 +14,24 @@ public class RegistrationServlet extends HttpServlet {
         // SOAP
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<h2>Your details have been recorded successfully! </h2>");
 
         // Form - data read
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+        String terms = req.getParameter("terms");
 
         // JDBC
 
-        out.println("<p>Full Name - " + firstName + " " + lastName + " </p>") ;
-        out.println("<p>Email - " + email + "</p>");
-        out.println("<p>Password - " + password + "</p>");
+        if(terms != null) {
+            out.println("<h2>Your details have been recorded successfully! </h2>");
+            out.println("<p>Full Name - " + firstName + " " + lastName + " </p>") ;
+            out.println("<p>Email - " + email + "</p>");
+            out.println("<p>Password - " + password + "</p>");
+            out.println("<p>Terms - " + terms + "</p>");
+        } else {
+            out.println("<h2>Your details has NOT been recorded, please agree to the conditions & try again! </h2>");
+        }
     }
 }
